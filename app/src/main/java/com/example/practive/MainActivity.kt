41f6c5c2941a2a8d111c86fd65ea32183033d519
sojin <dlthwls0110@gmail.com>
2024-01-28 19:04:47 +0900

    MainActivity.kt 수정

--- app/src/main/java/com/example/practive/MainActivity.kt
+++ app/src/main/java/com/example/practive/MainActivity.kt
@@ -36,6 +36,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initViewModel()
+        initRecyclerview()
+        initBtnAdd()
     }
 
     /*
